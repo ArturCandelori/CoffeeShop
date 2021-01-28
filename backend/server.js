203f56b313401faa92941f7e50cc244c5dfb0f5a
ArturCandelori <artur.candelori@hotmail.com>
2021-01-28 14:38:49 -0300

    preparando para deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import 'express-async-errors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
@@ -19,8 +20,24 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
